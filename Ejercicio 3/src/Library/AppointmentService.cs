f94b097d26c081a001db30d38ebdbadb1294e60d
Mauripache <mauri_pache@hotmail.com>
2020-09-01 21:03:11 -0300

    Comentarios expert srp

--- Ejercicio 3/src/Library/AppointmentService.cs
+++ Ejercicio 3/src/Library/AppointmentService.cs
@@ -3,6 +3,16 @@ using System.Text;
 
 namespace Library
 {
+    /*
+        No cumple SRP ya que si necesito cambiar cualquiera de las validaciones, o agregar mas
+        datos necesarios para el appointment debo modificar esta clase.
+        Movería los datos de name, id y phone number a una clase aparte ya que son todos datos
+        de la persona que hace el appointment. Moveria appointmentPlace a una
+        clase aparte ya que son datos del lugar del appointment. Movería el dato de doctorName 
+        a una clase Doctor ya que son datos del doctor. Haría estos cambios
+        ya que si no esta clase conocería datos que no están relacionados con un appointment de 
+        primera mano y no cumpliría el SRP.
+    */
     public class AppointmentService
     {
         public static string CreateAppointment(string name, string id, string phoneNumber, DateTime date, string appoinmentPlace, string doctorName)
