ca8f3510f811595df06ef28868d156be9e653d78
Mauripache <mauri_pache@hotmail.com>
2020-09-04 19:10:49 -0300

    fix

--- Ejercicio 2/src/Library/Libro.cs
+++ Ejercicio 2/src/Library/Libro.cs
@@ -4,10 +4,9 @@ namespace SRP
 {
     /*
         No se cumple el SRP ya que la clase Libro es responsable de conocer los datos del libro, pero además
-        de conocer el estante en que se encuentra y el sector de la biblioteca. Separaría en una clase a parte
-        llama Biblioteca que contiene una lista de objetos de una clase llamada Sector que contiene una lista 
-        de objetos de la clase Estante y la clase estante a su vez tendría
-        una lista de objetos de clase Libro.
+        de conocer el estante en que se encuentra y el sector de la biblioteca.La única responsabilidad del
+        libro es conocer datos de el y su única razón de cambio es agregarle nuevas propiedades.
+        Separaría en una clase a parte llamada Biblioteca que contiene una lista de libros y permite almacenarlos.
     */
     public class Libro
     {
